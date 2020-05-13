744161273c7e5ad221c1e2e3d005720ec2ef214b
Philip <philiprelander@gmail.com>
2020-05-13 22:53:21 +0300

    Reveal on scroll ready

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,13 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+
+var mobileMenu = new MobileMenu();
 
 if (module.hot) {
     module.hot.accept()
 }
 
-var mobileMenu = new MobileMenu();
